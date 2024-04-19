0ec24b2784fc8859bdcad4499136bb8f239c525e
Nicolas Costa Ribeiro <nribeiro.projetos@gmail.com>
2024-04-19 20:23:18 -0300

    feat: add cors npm module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Modules
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import connectDatabase from './src/database/db.js';
 import router from './src/routes/index.js';
@@ -7,6 +8,7 @@ import router from './src/routes/index.js';
 const app = express();
 
 connectDatabase();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
